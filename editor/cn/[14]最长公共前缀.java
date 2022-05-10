f20d05db642a1d1be2ae7b44c94aae6ec1183f58
lao.wan <1014074794@qq.com>
2022-05-10 22:24:26 +0800

    测试2

--- editor/cn/[14]最长公共前缀.java
+++ editor/cn/[14]最长公共前缀.java
@@ -33,7 +33,7 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-
+             return  null;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
