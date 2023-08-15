3a251e8170dfb7a25d77a012f917f4e064f8ed71
jack.wan <jack.wan@huolala.cn>
2023-08-15 16:47:51 +0800

    字符串字符全排序

--- editor/cn/[剑指 Offer 38]字符串的排列.java
+++ editor/cn/[剑指 Offer 38]字符串的排列.java
@@ -7,24 +7,24 @@ class Solution {
     public String[] permutation(String s) {
         c = s.toCharArray();
         dfs(0);
-        return res.toArray(new String(res.size()));
+        return res.toArray(new String[res.size()]);
     }
     
-    //回溯算法：深度优先
-    public void dfs(int x){
-        if(x = c.length - 1){  //长度满足，添加到结果集中
+    //回溯算法：深度优先，从首字母char[0]开始遍历
+    void dfs(int x){
+        if(x == c.length - 1){  //满足长度后，将字符串添加到结果集中
             res.add(String.valueOf(c));
             return;
         }
-        
-        Set<Character> set = new HashSet<>();
+
+        HashSet<Character> set = new HashSet<>();
         for (int i = x; i < c.length; i++) {
             if(set.contains(c[i])){//重复，枝剪
                 continue;
             }
             set.add(c[i]);
             
-            swap(i,x);//交换
+            swap(i,x);//交换,将char[i]固定在x位上
             dfs(x + 1);
             swap(i,x);//恢复交换
         }
@@ -32,7 +32,8 @@ class Solution {
     //交换
     void swap(int x,int y){
         char tmp = c[x];
-        
+        c[x] = c[y];
+        c[y] = tmp;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
