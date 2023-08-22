99824a220f6417cbda69200cde695dd210eb7256
jack.wan <jack.wan@huolala.cn>
2023-08-22 14:48:08 +0800

    数字序列中指定位的数字

--- editor/cn/[剑指 Offer 44]数字序列中某一位的数字.java
+++ editor/cn/[剑指 Offer 44]数字序列中某一位的数字.java
@@ -32,7 +32,28 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int findNthDigit(int n) {
+        //递推公式：
+        //digtit = digtit + 1
+        //start =  start * 10
+        //count = 9 * start * digit
+         //数位
+         int digit = 1;
+         //数位起始数字
+         long start = 1;
+         //数位总个数
+         long count = 9;
+        //1、确定 n 所在 数字 的 位数 ，记为 digit ；
+         while (n > count) {
+             n -= count;
+             digit += 1;
+             start *= 10;
+             count = digit * start * 9;
+         }
+        //2、确定 n 所在的 数字 ，记为 num ；
+        long num = start + (n - 1)/digit;
 
+        //3、确定 n 是 num 中的哪一数位，并返回结果。 最后的 -'0' 是为了字符转数字
+        return  Long.toString(num).charAt((n - 1) % digit) - '0';
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
